b3a623be4f22b7de18e09bf4dd4afd5a0e3ee986
deovinsum <gh@deovinsum.ru>
2024-02-23 23:21:36 +0300

    task 2

---
+++ Moonshot/CrewView.swift
@@ -0,0 +1,13 @@
+// Created by deovinsum
+
+import SwiftUI
+
+struct CrewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CrewView()
+}
